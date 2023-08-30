02fc3574251ed49d306e77fca43084f46c87f90f
Andrey Arzamaskov <arzamaskov@gmail.com>
2023-08-30 10:01:51 +0400

    create Calculator tests

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testCalculate(string $expression, float $expected): void
+    {
+        $calculator = new Calculator();
+
+        $this->assertEquals($expected, $calculator->calculate($expression));
+    }
+
+    /**
+     * @return array<string,array<int,mixed>>
+     */
+    public function dataProvider(): array
+    {
+        return [
+            '1 + 1' => [
+                '1 + 1',
+                2,
+            ],
+        ];
+    }
+}
